d4e42c74ed6868ea6e950a9209c9dc7fc88ed7c7
Gözde G <gzde.gurler@gmail.com>
2023-02-02 15:59:06 +0300

    13/13 passed

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,48 @@
 // HOKUS POKUS
+const express = require("express");
+
+const router = express.Router();
+const CarModels = require("./cars-model");
+const md = require("./cars-middleware");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const cars = await CarModels.getAll();
+    res.json(cars);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", md.checkCarId, async (req, res, next) => {
+  try {
+    res.json(req.car);
+  } catch (error) {
+    next(error);
+  }
+
+  //SOLUTION 2
+  // try {
+  //   const theCar = await CarModels.getById(req.params.id);
+  //   res.status(200).json(theCar);
+  // } catch (error) {
+  //   next(error);
+  // }
+});
+
+router.post(
+  "/",
+  md.checkCarPayload,
+  md.checkVinNumberUnique,
+  md.checkVinNumberValid,
+  async (req, res, next) => {
+    try {
+      const newCar = await CarModels.create(req.body);
+
+      res.status(201).json(newCar);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+module.exports = router;
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,15 @@
 // ESNEK
+exports.seed = async function (knex) {
+  await knex("cars").truncate();
+  await knex("cars").insert([
+    {
+      vin: "abcgd",
+      make: "Audi",
+      model: "RS 5",
+      mileage: 1000,
+      title: "clean",
+      transmission: "manual",
+    },
+    { vin: "abcgklkl", make: "Peugeot", model: "3008", mileage: 1200 },
+  ]);
+};
